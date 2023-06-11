b651abbd995462eadcc3394a404634ee9500ef15
Alexandre Ferreira <alexandrevmferreira@gmail.com>
2023-06-11 18:45:36 -0300

    correção-vídeos não estavam aparecendo

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -1,28 +1,27 @@
- import {conectaApi} from "./conectaApi.js";
- 
- const lista = document.querySelector("[data-lista]");
+import { conectaApi } from "./conectaApi.js";
 
- function constroiCard(titulo, descricao, url, imagem){
-    const video = document.createElement("li")
+const lista = document.querySelector("[data-lista]");
+
+function constroiCard(titulo, descricao, url, imagem) {
+    const video = document.createElement("li");
     video.className = "videos__item";
-    video.innerHTML = `
-        <iframe width="100%" height="72%" src="${url}"
-        title="${titulo}" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
-    <div class="descricao-video">
-        <img src="${imagem}" alt="logo canal alura">
-        <h3>${titulo}a</h3>
-        <p>${descricao}</p>
-    </div>  
-    `
-    return video;
- }
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+<div class="descricao-video">
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
+</div>`
 
- async function listaVideos(){
+    return video;
+}
+
+async function listaVideos() {
     const listaApi = await conectaApi.listaVideos();
     listaApi.forEach(elemento => lista.appendChild(
-    constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
- }
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+}
 
- listaVideos();
+listaVideos();
