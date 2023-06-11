c4158b0e6e352f3d33e857f036c873c806b0f570
Alexandre Ferreira <alexandrevmferreira@gmail.com>
2023-06-11 19:09:44 -0300

    criando evento para o botão de pesquisa no search de vídeos.

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,5 +1,12 @@
 import { conectaApi } from "./conectaApi.js";
 
-async function buscarVideo() {
-    const busca = await conectaApi.buscaVideo();
+async function buscarVideo(evento) {
+    evento.preventDefault();
+    
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").ariaValueMax;
+    const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 }
+
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
+
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
